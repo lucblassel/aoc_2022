51f2eadbb69337cdfe2ca00b782689a95ddb9f45
Luc Blassel <luc.blassel@gmail.com>
2022-12-11 15:33:54 +0100

    Day 11 (Remix)

--- 11/src/main.rs
+++ 11/src/main.rs
@@ -1,165 +1,127 @@
 #[derive(Debug, Clone)]
 struct Monkey {
     items: Vec<u64>,
-    operation: fn(u64) -> u64,
+    operator: char,
+    value: Option<u64>,
     test: u64,
     result: (usize, usize),
 }
 
 impl Monkey {
+    fn operation(&self, old: u64) -> u64 {
+        match self.operator {
+            '*' => match self.value {
+                Some(val) => old * val,
+                None => old * old,
+            },
+            '+' => match self.value {
+                Some(val) => old + val,
+                None => unreachable!("Cannot add old to old"),
+            },
+            _ => unreachable!("Unkown operator {}", self.operator),
+        }
+    }
+
     fn empty_items(&mut self) {
         self.items = vec![];
+    }
+
+    fn from_vec(vec: Vec<&str>) -> Self {
+        let items: Vec<u64> = vec[1]
+            .split_at(18)
+            .1
+            .split(", ")
+            .into_iter()
+            .map(|v| v.parse().unwrap())
+            .collect();
+        let test: u64 = vec[3].split_at(21).1.parse().unwrap();
+        let target_true: usize = vec[4].split_at(29).1.parse().unwrap();
+        let target_false: usize = vec[5].split_at(30).1.parse().unwrap();
+
+        let op_s = vec[2].split_at(23).1;
+        let (op, val_s) = op_s.split_at(1);
+        let operator = op.chars().collect::<Vec<char>>()[0];
+        let value = match val_s.trim().parse::<u64>() {
+            Ok(val) => Some(val),
+            Err(_) => None, // Use 'old' as the value
+        };
+
+        Monkey {
+            items,
+            operator,
+            value,
+            test,
+            result: (target_true, target_false),
+        }
     }
 }
 
 fn main() {
-
     println!("Day 11:");
-    // let mut monkeys: Vec<Monkey> = vec![
-    //     Monkey {
-    //         items: vec![79, 98],
-    //         operation: |old| old * 19,
-    //         test: 23,
-    //         result: (2, 3),
-    //     },
-    //     Monkey {
-    //         items: vec![54, 65, 75, 74],
-    //         operation: |old| old + 6,
-    //         test: 19,
-    //         result: (2, 0),
-    //     },
-    //     Monkey {
-    //         items: vec![79, 60, 97],
-    //         operation: |old| old * old,
-    //         test: 13,
-    //         result: (1, 3),
-    //     },
-    //     Monkey {
-    //         items: vec![74],
-    //         operation: |old| old + 3,
-    //         test: 17,
-    //         result: (0, 1),
-    //     },
-    // ];
+    let input = include_str!("../inputs/example.txt");
+    let mut line_buf = vec![];
 
-    let mut monkeys = vec![
-        Monkey {
-            items: vec![74, 64, 74, 63, 53],
-            operation: |old| old * 7,
-            test: 5,
-            result: (1, 6),
-        },
-        Monkey {
-            items: vec![69, 99, 95, 62],
-            operation: |old| old * old,
-            test: 17,
-            result: (2, 5),
-        },
-        Monkey {
-            items: vec![59, 81],
-            operation: |old| old + 8,
-            test: 7,
-            result: (4, 3),
-        },
-        Monkey {
-            items: vec![50, 67, 63, 57, 63, 83, 97],
-            operation: |old| old + 4,
-            test: 13,
-            result: (0, 7),
-        },
-        Monkey {
-            items: vec![61, 94, 85, 52, 81, 90, 94, 70],
-            operation: |old| old + 3,
-            test: 19,
-            result: (7, 3),
-        },
-        Monkey {
-            items: vec![69],
-            operation: |old| old + 5,
-            test: 3,
-            result: (4, 2),
-        },
-        Monkey {
-            items: vec![54, 55, 58],
-            operation: |old| old + 7,
-            test: 11,
-            result: (1, 5),
-        },
-        Monkey {
-            items: vec![79, 51, 83, 88, 93, 76],
-            operation: |old| old * 3,
-            test: 2,
-            result: (0, 6),
-        },
+    let mut monkeys: Vec<Monkey> = vec![];
+
+    for (_, line) in input.lines().enumerate() {
+        if line.is_empty() {
+            monkeys.push(Monkey::from_vec(line_buf));
+            line_buf = vec![];
+            continue;
+        }
+        line_buf.push(line);
+    }
+    monkeys.push(Monkey::from_vec(line_buf));
+
+    let modulo: u64 = monkeys.iter().map(|monkey| monkey.test).product();
+
+    let part_config: Vec<(Vec<Monkey>, u32, bool, u64)> = vec![
+        (monkeys.clone(), 20, true, 3),
+        (monkeys, 10_000, false, modulo),
     ];
 
-    let p2_modulo: u64 = monkeys.iter().map(|monkey| monkey.test).product();
+    for (i, (monkeys, turns, divide, operand)) in part_config.into_iter().enumerate() {
+        let mut monkeys = monkeys;
+        let mut activity = vec![0; monkeys.len()];
 
-    let mut monkeys_2 = monkeys.clone();
+        for _ in 1..=turns {
+            for i in 0..monkeys.len() {
+                let (new_worries, actions) = do_turn(&monkeys[i], divide, operand);
 
-    let mut activity = vec![0; monkeys.len()];
+                for (item, target) in new_worries {
+                    monkeys[target].items.push(item);
+                }
 
-    for _ in 1..=20 {
-        for i in 0..monkeys.len() {
-            let mut new_worries: Vec<(u64, usize)> = vec![];
-
-            for item in monkeys[i].items.iter() {
-                let new_worry = (monkeys[i].operation)(*item) / 3;
-                let target = if new_worry % monkeys[i].test == 0 {
-                    monkeys[i].result.0
-                } else {
-                    monkeys[i].result.1
-                };
-                new_worries.push((new_worry, target));
-                activity[i] += 1;
+                activity[i] += actions;
+                monkeys[i].empty_items();
             }
-
-            for (item, target) in new_worries {
-                monkeys[target].items.push(item);
-            }
-
-            monkeys[i].empty_items();
         }
+
+        activity.sort();
+        let answer: u64 = activity.iter().rev().take(2).product();
+
+        println!("\t{}) {answer}", i + 1);
     }
-
-    activity.sort();
-    let answer_1: u64 = activity.iter().rev().take(2).product();
-
-    println!("\t1) {answer_1}");
-
-    let mut activity = vec![0; monkeys_2.len()];
-
-    for _ in 1..=10000 {
-        for i in 0..monkeys_2.len() {
-            let mut new_worries: Vec<(u64, usize)> = vec![];
-
-            for item in monkeys_2[i].items.iter() {
-                let new_worry = (monkeys_2[i].operation)(*item) % p2_modulo;
-                let target = if new_worry % monkeys[i].test == 0 {
-                    monkeys_2[i].result.0
-                } else {
-                    monkeys_2[i].result.1
-                };
-                new_worries.push((new_worry, target));
-                activity[i] += 1;
-            }
-
-            for (item, target) in new_worries {
-                monkeys_2[target].items.push(item);
-            }
-
-            monkeys_2[i].empty_items();
-        }
-    }
-
-    activity.sort();
-    let answer_2: u64 = activity.iter().rev().take(2).product();
-
-    println!("\t2) {answer_2}");
 }
 
-fn print_monkeys(monkeys: &[Monkey]) {
-    for (i, monkey) in monkeys.iter().enumerate() {
-        println!("Monkey {i}: {:?}", monkey.items);
+fn do_turn(monkey: &Monkey, divide: bool, operand: u64) -> (Vec<(u64, usize)>, u64) {
+    let mut new_worries: Vec<(u64, usize)> = vec![];
+    let mut activity = 0;
+
+    for item in monkey.items.iter() {
+        let new_worry = if divide {
+            monkey.operation(*item) / operand
+        } else {
+            monkey.operation(*item) % operand
+        };
+        let target = if new_worry % monkey.test == 0 {
+            monkey.result.0
+        } else {
+            monkey.result.1
+        };
+        new_worries.push((new_worry, target));
+        activity += 1;
     }
+
+    (new_worries, activity)
 }
